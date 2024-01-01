88e49afb95571a9f1993baa0d809cad252fbb44d
Echa Apriliyanto <apriliyantoecha1@gmail.com>
2024-01-01 17:38:17 +0700

    Renaming fucntion in routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask import request
 # route index
 @app.route('/', methods=['GET'])
 def index():
-  return BookController.index(request)
+  return UserController.index(request)
 
 # login route for user
 @app.route('/login', methods=['POST'])
@@ -30,27 +30,27 @@ def dashboard():
 # route get all books
 @app.route('/books', methods=['GET'])
 def books():
-  return BookController.books(request)
+  return BookController.get_all(request)
 
 # route get book by id
 @app.route('/book/<id>', methods=['GET'])
 def book(id):
-  return BookController.book(request, id)
+  return BookController.get(request, id)
 
 # route create book
 @app.route('/book', methods=['POST'])
 def create_book():
-  return BookController.create_book(request)
+  return BookController.create(request)
 
 # route update book
 @app.route('/book/<id>', methods=['PUT'])
 def update_book(id):
-  return BookController.update_book(request, id)
+  return BookController.update(request, id)
 
 # route delete book
 @app.route('/book/<id>', methods=['DELETE'])
 def delete_book(id):
-  return BookController.delete_book(request, id)
+  return BookController.delete(request, id)
 
 # route search book
 @app.route('/search', methods=['GET'])
